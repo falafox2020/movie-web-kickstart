6e1eb0263ea64f4133b6282103bd4df8f7ed3fec
falafox2020 <88835390+falafox2020@users.noreply.github.com>
2024-10-09 18:52:28 +0800

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -5,7 +5,7 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://vidsrc.xyz/embed/movie/${id}`} />;
+  return <EmbedPlayer url={`https://vidsrc.me/v2/embed/movie/${id}`} />;
 }
 
 //https://vidsrc.xyz/embed/movie/tt5433140
